7180405c8d6c013195bc1a44bf85ac4bc29abeae
vkavith <vkavith@gmail.com>
2025-01-26 10:25:04 -0500

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,9 +4,12 @@ function Homepage() {
   const navigate = useNavigate();
 
   return (
-    <div className="button-container">
-      <button onClick={() => navigate("/search")}>Meals Search</button>
-      <button onClick={() => navigate("/area")}>Meals by Area</button>
+    <div>
+      <h1 className="welcome-title">Welcome to Meals Website</h1>
+      <div className="button-container">
+        <button onClick={() => navigate("/search")}>Meals Search</button>
+        <button onClick={() => navigate("/area")}>Meals by Area</button>
+      </div>
     </div>
   );
 }
